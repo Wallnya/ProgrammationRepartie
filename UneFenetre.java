59236e405ac2804500fd2cc491f78d2d2c7fa92e
Wallnya <34969114+Wallnya@users.noreply.github.com>
2019-02-01 15:23:02 +0100

    V2 + carre bouge

--- UneFenetre.java
+++ UneFenetre.java
@@ -5,26 +5,16 @@ public class UneFenetre extends JFrame {
     private final int LARG=400, HAUT=250;
     
     public UneFenetre(){
-    	// TODO
     	super("Mobile");
     	this.setSize(LARG,HAUT);
     	this.setVisible(true);
-    	
-    	
     	// ajouter sonMobile a la fenetre    
     	sonMobile = new UnMobile(LARG,HAUT);
     	this.add(sonMobile);
     	// creer une thread laThread avec sonMobile
     	Thread t = new Thread(sonMobile);
-
-    	// afficher la fenetre
-    	t.run();
-    	
+    	// afficher la fenetre    	
     	// lancer laThread 
     	t.start();
-    	
-    	
-    	
-
     }
 }
