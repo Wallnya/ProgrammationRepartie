e27150c2e1585fd1ddd84c5df1085f7c9f302da7
Wallnya <34969114+Wallnya@users.noreply.github.com>
2019-02-01 14:40:41 +0100

    V1

---
+++ UneFenetre.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class UneFenetre extends JFrame {
+    UnMobile sonMobile;
+    private final int LARG=400, HAUT=250;
+    
+    public UneFenetre()
+    {
+	// TODO 
+	// ajouter sonMobile a la fenetre
+	// creer une thread laThread avec sonMobile
+	// afficher la fenetre
+	// lancer laThread 
+    }
+}
