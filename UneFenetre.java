34f110aeffdafbb6b0926ae6bd9b1e2544f1036e
Wallnya <34969114+Wallnya@users.noreply.github.com>
2019-02-08 15:38:45 +0100

    Plusieurs carres

--- UneFenetre.java
+++ UneFenetre.java
@@ -10,15 +10,19 @@ public class UneFenetre extends JFrame implements ActionListener{
     UnMobile sonMobile;
     UnMobile sonMobile2;
 
-    private final int LARG=400, HAUT=250;
+    private final int LARG=800, HAUT=500;
     private Thread t;
     private Thread t2;
     private JButton sonBouton1;
     private JButton sonBouton2;
     private boolean etat = true;
     private boolean etat2 = true;
-
     
+    private UnMobile[] myMobile;
+    private Thread[] myThread;
+    private JButton[] myButton;
+    private boolean[] myState;
+       
     public UneFenetre(){
     	super("Mobile");
     	
@@ -27,8 +31,28 @@ public class UneFenetre extends JFrame implements ActionListener{
     	// ajouter sonMobile a la fenetre    
 
     	Container leConteneur = getContentPane();
-    	leConteneur.setLayout (new GridLayout(2, 2));
-    	sonMobile=new UnMobile(LARG/2, HAUT);
+    	leConteneur.setLayout (new GridLayout(10, 1));
+    	
+    	myMobile = new UnMobile[10];
+    	myThread = new Thread[10];
+    	myState = new boolean[10];
+    	myButton = new JButton[10];
+    	
+    	
+    	for (int i=0;i<10;i++){
+    		myMobile[i]=new UnMobile(LARG,HAUT/10);
+    		myButton[i]=new JButton ("Start/Stop");
+    		myButton[i].setActionCommand(Integer.toString((i)));
+    		myButton[i].addActionListener(this);
+    		leConteneur.add(myMobile[i]);
+    		leConteneur.add(myButton[i]);
+    		myThread[i] = new Thread(myMobile[i]);
+    		myState[i]=true;
+    	}
+    	for (int j=0;j<10;j++){
+    		myThread[j].start();
+    	}
+    	/*sonMobile=new UnMobile(LARG/2, HAUT);
     	sonBouton1= new JButton ("Start/Stop");
     	sonBouton1.addActionListener(this);
     	leConteneur.add(sonBouton1);
@@ -37,22 +61,33 @@ public class UneFenetre extends JFrame implements ActionListener{
     	sonBouton2= new JButton ("Start/Stop");
     	sonBouton2.addActionListener(this);
     	leConteneur.add(sonBouton2);
-    	leConteneur.add(sonMobile2);
+    	leConteneur.add(sonMobile2);*/
 
     	
     	// creer une thread laThread avec sonMobile
-    	t = new Thread(sonMobile);
-    	t2 = new Thread(sonMobile2);
-
+    	/*t = new Thread(sonMobile);
+    	t2 = new Thread(sonMobile2);*/
     	// afficher la fenetre    	
     	// lancer laThread 
-    	t.start();
-    	t2.start();
+    	/*t.start();
+    	t2.start();*/
     }
 
 	public void actionPerformed(ActionEvent parEvt) {
 		Object source = parEvt.getSource();
-		if(source == sonBouton1){
+		
+		if (source instanceof JButton){
+			int i = Integer.parseInt(parEvt.getActionCommand());
+			if (myState[i] == true){
+				myState[i] = false;
+				myThread[i].suspend();
+			}
+			else{
+				myState[i] = true;
+				myThread[i].resume();
+			}
+		}
+		/*if(source == sonBouton1){
 			if (etat == true){
 				etat = false;
 				t.suspend();
@@ -71,6 +106,6 @@ public class UneFenetre extends JFrame implements ActionListener{
 				etat2 = true;
 				t2.resume();
 			}
-		}
+		}*/
 	}
 }
