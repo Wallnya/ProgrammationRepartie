3afa7c4112d14475ec396764ba3b2d0ce90c9702
Wallnya <34969114+Wallnya@users.noreply.github.com>
2019-02-08 14:39:29 +0100

    Utilisation de sémaphore/mutex

--- TP2_Affichage/Affichage.java
+++ TP2_Affichage/Affichage.java
@@ -4,17 +4,17 @@ import java.lang.String;
 
 public class Affichage extends Thread{
 	String texte;
-	static Exclusion exclusionIm = new Exclusion();
+	//static Exclusion exclusionIm = new Exclusion();
+	static semaphoreBinaire mutex = new semaphoreBinaire(1);
 	public Affichage (String txt){texte=txt;}
 	
 	public void run(){
-
-		//Section critique, c'est le for
-		synchronized(exclusionIm){
+			//Section critique, c'est le for
+			mutex.syncWait();
 			for (int i=0; i<texte.length(); i++){
 			    System.out.print(texte.charAt(i));
 			    try {sleep(100);} catch(InterruptedException e){};
 			}//Fin
-		}
+			mutex.syncSignal();
 	}
 }
--- TP2_Affichage/semaphoreBinaire.java
+++ TP2_Affichage/semaphoreBinaire.java
@@ -1,12 +1,13 @@
+package v2;
 
 public final class semaphoreBinaire extends semaphore {
 public semaphoreBinaire(int valeurInitiale){
 	super((valeurInitiale != 0) ? 1:0);
-	//System.out.print(valeurInitiale);
+	System.out.println("Je rentre en section critique : "+valeurInitiale);
 }
 public final synchronized void syncSignal(){
 	super.syncSignal();
-	//System.out.print(valeur);
+	System.out.println("Je sors de section critique : "+valeur);
 	if (valeur>1) valeur = 1;
 }
 }
